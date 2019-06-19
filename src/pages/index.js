31db19f57007cf0373e82123cce7a5ca6ae3063e
jackding96 <jackding96@outlook.com>
2019-06-19 11:14:36 -0400

    initial page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,22 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+export default () => (<div>
+  <div>Jack Ding is a Software Developer</div>
+  <div>Previously at Lyft, Rangle.IO, and IBM Cloud</div>
+  <div>Loves coffee</div>
+
+  <div>
+    <ul>
+      <li>
+        <a>Email</a>
+      </li>
+      <li>
+        <a>GitHub</a>
+      </li>
+      <li>
+        <a>LinkedIn</a>
+      </li>            
+    </ul>
+
+  </div>
+</div>);
