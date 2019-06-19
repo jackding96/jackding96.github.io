ec9d258138c8bb01750b14a73a399db75004cf41
jackding96 <jackding96@outlook.com>
2019-06-19 12:33:42 -0400

    profile image

---
+++ src/components/profileImage.js
@@ -0,0 +1,9 @@
+import React from "react"
+import meImg from "../../public/static/me.jpg"
+import profileImageStyles from "./profileImage.module.css"
+
+export default () => (<div>
+  <div>
+    <img src={meImg} />
+  </div>
+</div>);
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,9 @@
 import React from "react"
 import Links from "../components/links.js"
+import ProfileImage from "../components/profileImage.js"
 
 export default () => (<div>
+  <ProfileImage/>
   <div>Jack Ding is a Software Developer.</div>
   <div>Previously at Lyft, Rangle.IO, and IBM Cloud.</div>
   <div>Loves coffee.</div>
