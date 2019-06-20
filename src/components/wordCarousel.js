b250689c70480114bd3a1a4057f048788b743afd
jackding96 <jackding96@outlook.com>
2019-06-20 18:55:31 -0400

    carousel word more prog

--- src/components/wordCarousel.js
+++ src/components/wordCarousel.js
@@ -5,28 +5,44 @@ class WordCarousel extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      current: 1,
+      list: ['a','b','c','d'],
       firstPos: wordCarouselStyles.current,
-      firstText: 'Software Developer.',
+      firstText: '',
       secondPos: wordCarouselStyles.after,
-      secondText: 'Code Connoisseur.',
+      secondText: '',
     };
-
+    this.i = 0;
+    this.current = 1;
     this.changeTitle = this.changeTitle.bind(this);
   }
   componentDidMount() {
+    this.setState({firstText: this.state.list[this.i]});
     setInterval(this.changeTitle, 3000);
   }
   changeTitle() {
     this.setState({secondPos: this.state.secondPos == wordCarouselStyles.current ? wordCarouselStyles.after : wordCarouselStyles.current})
     this.setState({firstPos: this.state.firstPos == wordCarouselStyles.current ? wordCarouselStyles.after : wordCarouselStyles.current})
+    
+    this.i++;
+    if (this.current == 1) { // Currently, "first" is the current, "second" is moving in
+      this.setState({ secondText : this.state.list[this.i % this.state.list.length] });
+    } else { // Currently, "second" is the current, "first" is moving in
+      this.setState({ firstText: this.state.list[this.i % this.state.list.length] });
+    }
+    this.current = this.current*-1;
+    
   }
   render() {
     return (
       <div className={wordCarouselStyles.test}>
-        <span className={`${wordCarouselStyles.title} ${this.state.secondPos}`}>{this.state.secondText}</span>
-        <span className={`${wordCarouselStyles.title} ${this.state.firstPos}`}>{this.state.firstText}</span>
+        <span className={`${wordCarouselStyles.title} ${this.state.secondPos}`}>
+          {this.state.secondText}
+        </span>
+        <span className={`${wordCarouselStyles.title} ${this.state.firstPos}`}>
+          {this.state.firstText}
+        </span>
       </div>
-      
     );
   }
 }
