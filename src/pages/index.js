f996a59387003c787478640c56363bd216b3ef7a
jackding96 <jackding96@outlook.com>
2019-06-25 16:52:13 -0400

    used flexbox for content alignment

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,32 +2,50 @@ import React from "react"
 import Links from "../components/links.js"
 import ProfileImage from "../components/profileImage.js"
 import WordCarousel from "../components/wordCarousel.js"
+import indexStyles from './index.module.css'
 
-export default () => (<div>
-  <ProfileImage/>
+export default () => (<div className={indexStyles.container}>
 
-  {/* First Line */}
-  <div>
-    <div><span className={'highlight'}>Jack Ding</span> is a</div>
-    <WordCarousel
-      words={['Software Engineer.', 'Code Connoisseur.', 'Technology Templar.', 'Binary Bandit.', 'Programming Pirate.', 'Algorithm Antihero.', 'Design Darth Vadar.', 'HPC Heavyweight.']}
-      slideFrom='above'
-    />
+  {/* Profile Image Container */}
+  <div className={indexStyles.profileImageContainer}>
+    <ProfileImage />
+  </div>
+  
+  {/* Main Content Container */}
+  <div className={indexStyles.contentContainer}>
+
+    {/* First Line */}
+    <div className={indexStyles.contentLineContainer}>
+      <div className={indexStyles.contentLine}><span className={'highlight'}>Jack Ding</span> is a</div>
+      <div className={indexStyles.carouselContainer}>
+        <WordCarousel
+          words={['Software Engineer.', 'Code Connoisseur.', 'Technology Templar.', 'Binary Bandit.', 'Programming Pirate.', 'Algorithm Antihero.', 'Design Darth Vadar.', 'HPC Heavyweight.']}
+          slideFrom='above'
+        />
+      </div>
+    </div>
+
+    {/* Second Line */}
+    <div className={indexStyles.contentLineContainer}>
+      <div>Previously at <span className={'highlight'}>Lyft</span>, <span className={'highlight'}>Rangle.IO</span>, and <span className={'highlight'}>IBM Cloud</span>.</div>
+    </div>
+
+    {/* Third Line */}
+    <div className={indexStyles.contentLineContainer}>
+      <div className={indexStyles.contentLine}>Loves</div>
+      <div className={indexStyles.carouselContainer}>
+        <WordCarousel
+          words={['Coffee.', 'Taming Lions.', 'Critiquing Impressionist Art.', 'Knocking Down Drywall.', 'Freshly Cut Lawns.', 'Building Gingerbread Houses.', 'Growing Tomatoes.', 'Finding Lost Treasure.', 'Long Walks On The Beach.']}
+          slideFrom='below'
+        />
+      </div>
+    </div>
+
   </div>
 
-  {/* Second Line */}
-  <div>
-    Previously at <span className={'highlight'}>Lyft</span>, <span className={'highlight'}>Rangle.IO</span>, and <span className={'highlight'}>IBM Cloud</span>.
+  {/* Links Container */}
+  <div className={indexStyles.linksContainer}>
+    <Links/>
   </div>
 
-  {/* Third Line */}
-  <div>
-    <div>Loves</div>
-    <WordCarousel
-      words={['Coffee.', 'Taming Lions.', 'Critiquing Impressionist Art.', 'Knocking Down Drywall.', 'Freshly Cut Lawns.', 'Building Gingerbread Houses.', 'Growing Tomatoes.', 'Finding Lost Treasure.', 'Long Walks On The Beach.']}
-      slideFrom='below'
-    />
-  </div>
-
-  <Links/>
 </div>);
